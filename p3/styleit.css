41fe910a1486f040f9618594dc73489a3ad9b975
amharrington <amharrington@massart.edu>
2015-03-23 14:06:14 -0400

    draw

---
+++ p3/styleit.css
@@ -0,0 +1,32 @@
+
+/* Add Styles Here */
+
+
+function flower(size){
+forward(size);
+rotate(120);
+forward(size);
+rotate(120);
+forward(size+size);
+rotate(120);
+forward(size);
+rotate(120);
+forward(size+size);
+rotate(120);
+forward(size);
+rotate(120);
+forward(size);
+}
+
+for(var x = 0; x < 5; x++){
+  speed(100);
+  flower(100);
+  move(100,0);
+}
+  for(var x = 0; x < 5; x++){
+  speed(100);
+  flower(100);
+  move(100,0);
+  
+  
+}
