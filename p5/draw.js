27b4334213ef06ae4bcd32d4c7c3a0637dc82c56
amharrington <amharrington@massart.edu>
2015-03-30 14:44:04 -0400

    review

--- p5/draw.js
+++ p5/draw.js
@@ -1,4 +1,4 @@
-function addPerson(name,loc,image) {
+/*function addPerson(name,loc,image) {
  var $container = $("div.container");
  var $div = $("<div>").addClass("person");
  var $name = $("<h1>").text(name);
@@ -9,33 +9,3 @@ $div.append($name).append($loc).append($image);
 
 $container.append($div);
 }
-
-
-addPerson("Bob Johnson","Boston, MA", "http://lorempixel.com/200/200/people/2/");
-addPerson("Frank Scott","San Diego, CA", "http://lorempixel.com/200/200/people/3/");
-addPerson("Amy Smith","New York, NY", "http://lorempixel.com/200/200/people/4/");
-addPerson("Amy Smith","New York, NY", "http://lorempixel.com/200/200/people/4/");
-
-
-
-/*
-addImage("http://scontent.cdninstagram.com/hphotos-xaf1/t51.2885-15/s150x150/e15/11055772_1558157594448054_879696703_n.jpg",
-        "https://instagram.com/p/0lOFZ0g8gA/",
-        720, "منشن الي تبيه يشوف المقطع");
-        
-addImage("http://scontent.cdninstagram.com/hphotos-xpa1/t51.2885-15/s150x150/e15/924442_823400811060434_669939277_n.jpg",
-        "https://instagram.com/p/0lR2EBzUie/",
-        432, "Aquela buzinadinha");
-        
-addImage("http://scontent.cdninstagram.com/hphotos-xfa1/t51.2885-15/s150x150/e15/10919675_944043618953681_1961747465_n.jpg",
-        "https://instagram.com/p/0lTX-pqkcC/",
-        543, "Сегодня была на прекрасном мероприятии #soundkids");
-        
-addImage("http://scontent.cdninstagram.com/hphotos-xaf1/t51.2885-15/s150x150/e15/11055616_1568588500090432_546823562_n.jpg",
-        "https://instagram.com/p/0lNgG4st5V/",
-        344, "Are your tubes in good shape?");
-*/
-        
-
-
-        
