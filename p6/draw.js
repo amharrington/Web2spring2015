98eee98ecf04799f75edb010f96354325cbfb43e
amharrington <amharrington@massart.edu>
2015-04-06 13:17:24 -0400

    uhhhh

---
+++ p6/draw.js
@@ -0,0 +1,11 @@
+/*function addPerson(name,loc,image) {
+ var $container = $("div.container");
+ var $div = $("<div>").addClass("person");
+ var $name = $("<h1>").text(name);
+ var $loc = $("<h3>").text(loc);
+ var $image = $("<img>").attr("src",image);
+ 
+$div.append($name).append($loc).append($image);
+
+$container.append($div);
+}
