b05631d0ed5f130ce978b3a855da6a8af12d3265
amharrington <amharrington@massart.edu>
2015-04-13 15:31:51 -0400

    scroll

--- EXERCISE2/draw.js
+++ EXERCISE2/draw.js
@@ -3,3 +3,8 @@ $(".scrolling").scrollFlight();
 $("h1").on("arrived",function(e) {
   $(this).addClass("on-page");
 });
+
+
+$(".iframe-cover").on("arriving",function(e) {
+  $(this).addClass("appeared");
+});
