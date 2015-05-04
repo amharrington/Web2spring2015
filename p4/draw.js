185b8ff1556db9dca26878c1c2fdd50ebfbcf870
amharrington <amharrington@massart.edu>
2015-05-04 14:16:38 -0400

    ok

--- p4/draw.js
+++ p4/draw.js
@@ -2,76 +2,29 @@
 function addImage(name,likes,image) {
  var $container = $("div.container");
  var $div = $("<div>").addClass("person");
+ var $image = $("<img>").attr("src",image);
  var $name = $("<h1>").text(name);
  var $likes = $("<h3>").text(likes);
- var $image = $("<img>").attr("src",image);
  
-$div.append($name).append($likes).append($image);
+
+$($likes).css({
+	"position":"relative",
+	"left":"70px"
+});
+
+$div.append($image).append($name).append($likes);
 
 $container.append($div);
 }
 
 
-addImage("Bob Johnson","720", "http://lorempixel.com/200/200/people/2/");
-addImage("Frank Scott","432", "http://lorempixel.com/200/200/people/3/");
-addImage("Amy Smith","543", "http://lorempixel.com/200/200/people/4/");
-addImage("Amy Smith","344", "http://lorempixel.com/200/200/people/4/");
+addImage("منشن الي تبيه يشوف المقطع", "720", "http://scontent.cdninstagram.com/hphotos-xaf1/t51.2885-15/s150x150/e15/11055772_1558157594448054_879696703_n.jpg",
+        "https://instagram.com/p/0lOFZ0g8gA/");
+addImage("RCF x VFS2 - pic via @kdot_vossen #FreeBuju #Vossen #CarsWithoutLimits","432", "http://scontent.cdninstagram.com/hphotos-xaf1/t51.2885-15/s150x150/e15/11055442_1387234548264057_209258360_n.jpg",
+         "https://instagram.com/p/0lS-LWTbw9/");
+addImage("Сегодня была на прекрасном мероприятии #soundkids","543", "http://scontent.cdninstagram.com/hphotos-xfa1/t51.2885-15/s150x150/e15/10919675_944043618953681_1961747465_n.jpg",
+       "https://instagram.com/p/0lTX-pqkcC/");
+addImage("Are your tubes in good shape?","344", "http://scontent.cdninstagram.com/hphotos-xaf1/t51.2885-15/s150x150/e15/11055616_1568588500090432_546823562_n.jpg",
+        "https://instagram.com/p/0lNgG4st5V/");
 
-function addImage(name,likes,image) {
- var $container = $("div.container");
- var $div = $("<div>").addClass("person");
- var $name = $("<h1>").text(name);
- var $likes = $("<h3>").text(likes);
- var $image = $("<img>").attr("src",image);
- 
-$div.append($name).append($likes).append($image);
-
-$container.append($div);
-}
-
-
-addImage("Bob Johnson","720", "http://lorempixel.com/200/200/people/2/");
-addImage("Frank Scott","432", "http://lorempixel.com/200/200/people/3/");
-addImage("Amy Smith","543", "http://lorempixel.com/200/200/people/4/");
-addImage("Amy Smith","344", "http://lorempixel.com/200/200/people/4/");
-
-function addImage(name,likes,image) {
- var $container = $("div.container");
- var $div = $("<div>").addClass("person");
- var $name = $("<h1>").text(name);
- var $likes = $("<h3>").text(likes);
- var $image = $("<img>").attr("src",image);
- 
-$div.append($name).append($likes).append($image);
-
-$container.append($div);
-}
-
-
-addImage("Bob Johnson","720", "http://lorempixel.com/200/200/people/2/");
-addImage("Frank Scott","432", "http://lorempixel.com/200/200/people/3/");
-addImage("Amy Smith","543", "http://lorempixel.com/200/200/people/4/");
-addImage("Amy Smith","344", "http://lorempixel.com/200/200/people/4/");
-
-
-/*
-addImage("http://scontent.cdninstagram.com/hphotos-xaf1/t51.2885-15/s150x150/e15/11055772_1558157594448054_879696703_n.jpg",
-        "https://instagram.com/p/0lOFZ0g8gA/",
-        720, "منشن الي تبيه يشوف المقطع");
-        
-addImage("http://scontent.cdninstagram.com/hphotos-xpa1/t51.2885-15/s150x150/e15/924442_823400811060434_669939277_n.jpg",
-        "https://instagram.com/p/0lR2EBzUie/",
-        432, "Aquela buzinadinha");
-        
-addImage("http://scontent.cdninstagram.com/hphotos-xfa1/t51.2885-15/s150x150/e15/10919675_944043618953681_1961747465_n.jpg",
-        "https://instagram.com/p/0lTX-pqkcC/",
-        543, "Сегодня была на прекрасном мероприятии #soundkids");
-        
-addImage("http://scontent.cdninstagram.com/hphotos-xaf1/t51.2885-15/s150x150/e15/11055616_1568588500090432_546823562_n.jpg",
-        "https://instagram.com/p/0lNgG4st5V/",
-        344, "Are your tubes in good shape?");
-*/
-        
-
-
-        
+    
